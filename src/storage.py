d5e63ac2b6762596792d40380c47cae951a2293b
GregoryKogan <grisha.koganovskiy@gmail.com>
2023-02-13 04:15:20 +0300

    Storage encryption added

--- src/storage.py
+++ src/storage.py
@@ -1,22 +1,36 @@
 import json
 import os
+from aes_cipher import AESCipher
 
 
 class Storage:
-    def __init__(self, filename: str = "storage.json") -> None:
+    def __init__(self, encryption_key: str, filename: str = "storage.txt") -> None:
+        self.encryption_key = encryption_key
+        self._data = {}
+
         self._filename = os.path.join(os.path.dirname(__file__), filename)
-        if not os.path.exists(self._filename):
-            with open(self._filename, "w") as storage:
-                storage.write("{}")
+        if os.path.exists(self._filename):
+            self.load()
+        else:
+            self.save()
+
+    def __repr__(self) -> str:
+        return f"<Storage {json.dumps(self._data)}>"
+
+    def save(self):
+        with open(self._filename, "w") as storage:
+            storage.write(AESCipher.encrypt(self.encryption_key, json.dumps(self._data)))
+
+    def load(self):
+        with open(self._filename, "r") as storage:
+            self._data = json.loads(AESCipher.decrypt(self.encryption_key, storage.read()))
 
     def write(self, key: str, value: object) -> None:
-        with open(self._filename, "r") as f:
-            data = json.load(f)
-        data[key] = value
-        with open(self._filename, "w") as f:
-            json.dump(data, f)
+        self._data[key] = value
+        self.save()
 
     def read(self, key: str) -> object:
-        with open(self._filename, "r") as f:
-            data = json.load(f)
-        return data.get(key)
+        return self._data.get(key)
+
+    def stores_key(self, key: str) -> bool:
+        return key in self._data
